b1eef33b9ce7719a5f846983e1f282805fe682d7
jnproyects <judith1912black@gmail.com>
2023-09-15 15:28:32 -0500

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,20 @@
+
+const { io } = require('../index');
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    // client.on('event', data => { /* … */ });
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje:', payload );
+
+        io.emit('mensajeTodos', { admin: 'Nuevo mensaje' });
+
+    });
+
+});
